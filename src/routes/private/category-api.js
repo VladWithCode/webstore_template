b5a0a9b326f6771694e918d4a1ca5547d41cba84
vladwithcode <vladwithcode@gmail.com>
2021-10-16 16:23:21 -0500

    Added controllers to endpoints

--- src/routes/private/category-api.js
+++ src/routes/private/category-api.js
@@ -1,5 +1,21 @@
+const {
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require('../../controllers/category.ctrl');
+
 const router = require('express').Router();
 
-router.get('/');
+router.post('/', createCategory);
+
+router.get('/', getCategories);
+
+router.get('/:id', getCategory);
+
+router.put('/:id', updateCategory);
+
+router.delete('/:id', deleteCategory);
 
 module.exports = router;
