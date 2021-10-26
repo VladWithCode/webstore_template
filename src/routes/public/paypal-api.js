768a65b68ab512cf0609a36c4bffb895c83f52af
vladwithcode <vladwithcode@gmail.com>
2021-10-26 18:34:24 -0500

    Get Sale by Order ID

--- src/routes/public/paypal-api.js
+++ src/routes/public/paypal-api.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
-const { createOrder, captureOrder } = require('../../controllers/paypal.ctrl');
+const {
+  createOrder,
+  captureOrder,
+  getOrder,
+} = require('../../controllers/paypal.ctrl');
 
 router.post('/order', createOrder);
 
 router.post('/order/:orderID/capture', captureOrder);
+
+router.get('/order/:orderId', getOrder);
 
 module.exports = router;
